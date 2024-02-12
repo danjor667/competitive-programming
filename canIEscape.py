ccf64239b1036d9522746d7b5e9be4a30c58eae3
Jordan S. L. Nguepi <109290255+danjor667@users.noreply.github.com>
2024-02-12 22:21:03 +0200

    Create canIEscape.py

---
+++ canIEscape.py
@@ -0,0 +1,8 @@
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        my_dist = abs(0-target[0]) + abs(0-target[1])
+        for ghost in ghosts:
+            ghost_dist = abs(ghost[0]-target[0]) + abs(ghost[1]-target[1])
+            if ghost_dist <= my_dist:
+                return False
+        return True
